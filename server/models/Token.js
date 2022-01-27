ed91c7c3a0f4b0cc3bd4113927fedadcb52abf62
kostya zalilov <zalilov.k@gmail.com>
2022-01-27 16:36:40 +0500

    auth.routes.js & Tokens

--- server/models/Token.js
+++ server/models/Token.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
   user: {type: Schema.Types.ObjectId, ref: 'User'},
-  refrechToken: { type: String, required: true}
+  refreshToken: { type: String, required: true}
 }, {
   timestamps: true
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router( { mergeParams: true } )
 router.use('/auth', require('./auth.routes'))
 router.use('/articles', require('./articles.routes'))
 router.use('/comments', require('./comments.routes'))
+router.use('/user', require('./user.routes'))
 
 module.exports = router
